9767c2dd7305dd93f04921408918f756d150be44
Keerthi64-10 <kkrithika785@gmail.com>
2026-03-16 10:53:52 +0530

    Refactor Calculator to use fixed values for addition
    Replaced user input with fixed numbers for addition.

--- Calculator.java
+++ Calculator.java
@@ -1,52 +1,14 @@
-import java.util.Scanner;
-
 public class Calculator {
-
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("=== Simple Calculator ===");
-
-        System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
-
-        System.out.println("Choose operation (+, -, *, /): ");
-        char operator = sc.next().charAt(0);
-
+        double num1 = 10;
+        double num2 = 5;
         double result;
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
+        result = num1 + num2;
 
-            case '-':
-                result = num1 - num2;
-                break;
-
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                if (num2 != 0)
-                    result = num1 / num2;
-                else {
-                    System.out.println("Cannot divide by zero!");
-                    return;
-                }
-                break;
-
-            default:
-                System.out.println("Invalid operator!");
-                return;
-        }
-
-        System.out.println("Result: " + result);
-        sc.close();
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Addition Result: " + result);
     }
 }
